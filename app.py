0a6d1f7cf5065b63911ea36ee9393d260bad3db8
sharmi <sharmilaakarthikeyan@gmail.com>
2026-01-03 19:29:15 +0530

    convert to flask web app

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
-print("Hello from DevOps")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello from DevOps Web App"
+
+if __name__ == "__main__":
+    app.run()
+
