b6978ae54074403b57976976961e2d9c1b161e53
sharmi <sharmilaakarthikeyan@gmail.com>
2026-01-10 08:16:26 +0530

    add healthcheck endpoint

--- app.py
+++ app.py
@@ -6,6 +6,11 @@ app = Flask(__name__)
 def home():
     return "Hello from DevOps Web App"
 
+@app.route("/health")
+def health():
+    return "OK"
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
+
 
