d70dd2ec53565adbce3bef301713a80c0fe7c718
sharmi <sharmilaakarthikeyan@gmail.com>
2026-01-20 19:58:38 +0530

    add github action workflow

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from DevOps Web App"
+    return "Auto deployed using CI/CD 🚀"
 
 @app.route("/health")
 def health():
-    return "OK"
+    return "OK ....."
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
